1c79f14a0882b82ed5f259d532db26c55ce77fe5
Kameshmahadev <mkameshdev@gmail.com>
2025-04-29 10:48:00 +0530

    commit

--- src/api/omdbService.js
+++ src/api/omdbService.js
@@ -1,6 +1,6 @@
 // src/api/omdbApi.js
 
-const API_KEY = 'c6bb7eb3'; // Replace with your OMDB API key
+const API_KEY = 'c6bb7eb3'; // 🔁 Replace with your actual OMDB API key
 const BASE_URL = 'https://www.omdbapi.com/';
 
 export const fetchMovies = async (query, page = 1, type = '') => {
